d79029314ad31848f1fd11053767ea188c2767a1
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-03-11 18:47:46 +0100

    update models

--- src/model.py
+++ src/model.py
@@ -1,11 +1,11 @@
 import os
 
+import numpy as np
 import openai
-import torch
 import torch.nn as nn
 from dotenv import load_dotenv
 
-# env
+# env variables
 load_dotenv(dotenv_path="../.env")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -29,13 +29,13 @@ def GPT_3(prompt):
         temperature=0.7,
         stop=["]"],
     )
-    return response["choices"][0]["text"]
+    return "[" + response["choices"][0]["text"] + "]"  # add brackets to make it a list
 
 
 class DivideAndConquerFNN(nn.Module):
     def __init__(self, input_dim, hidden_dim, number_of_labels):
         """
-        Feedforward neural network
+        A list of feedforward neural network, each is trained to predict one label in the output
         """
         super(DivideAndConquerFNN, self).__init__()
         self.seq = nn.ModuleList(
@@ -54,3 +54,18 @@ class DivideAndConquerFNN(nn.Module):
 
     def forward(self, x):
         return [classifier(x) for classifier in self.seq]
+
+
+def classifiers_fit(classifiers, x_train, y_train):
+    """Fit a list of classifiers each to classify one label in the output"""
+    assert len(classifiers) == y_train.shape[1]
+    for clf, y_label in zip(classifiers, y_train.T):
+        clf.fit(x_train, y_label)
+
+
+def classifiers_predict(classifiers, x_test):
+    """Predict the output of a list of classifiers"""
+    predictions = []
+    for clf in classifiers:
+        predictions.append(clf.predict(x_test))
+    return np.array(predictions).T
