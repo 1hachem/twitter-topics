03cd4f8a4f733ded86797cfbbae2f93832a54614
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-03-11 20:19:04 +0100

    fix loss and add fit function

--- src/train.py
+++ src/train.py
@@ -2,49 +2,57 @@ import torch
 from tqdm import tqdm
 
 
-def train(
+def train_fnns(
     model,
     train_loader,
     val_loader,
     optimizer,
-    criterion,
-    weights,
+    criterions,
     epochs,
     device="cpu",
 ):
     """
     Train a list feedforward neural networks, each is trained to predict one label in the output
-    the training criterion is the sum of the weighted losses of each classifier
     """
     for epoch in range(epochs):
         model.train()
         loop = tqdm(train_loader)
         for x, y in loop:
             x, y = x.to(device), y.to(device)
-            optimizer.zero_grad()
-            loss = sum(
-                [
-                    w * criterion(x_, torch.unsqueeze(y_, 1))
-                    for w, x_, y_ in zip(weights, model(x), y.T)
-                ]
-            )
 
-            loss.backward()
+            optimizer.zero_grad()
+
+            losses = [
+                criterion(x_, torch.unsqueeze(y_, 1))
+                for criterion, x_, y_ in zip(criterions, model(x), y.T)
+            ]
+            for loss in losses:
+                loss.backward()
+
             optimizer.step()
+            avg_loss = sum(losses) / len(losses)
 
             # validation
-            val_loss = 0
             model.eval()
-            for x, y in val_loader:
-                x, y = x.to(device), y.to(device)
-                val_loss += sum(
-                    [
-                        w * criterion(x_, torch.unsqueeze(y_, 1))
-                        for w, x_, y_ in zip(weights, model(x), y.T)
+            avg_val_loss = torch.tensor(0.0)
+            with torch.no_grad():
+                for x, y in val_loader:
+                    x, y = x.to(device), y.to(device)
+                    val_losses = [
+                        criterion(x_, torch.unsqueeze(y_, 1))
+                        for criterion, x_, y_ in zip(criterions, model(x), y.T)
                     ]
-                )
-            val_loss /= len(val_loader)
+                    avg_val_loss += sum(val_losses) / (
+                        len(val_losses) * len(val_loader)
+                    )
 
             loop.set_description(
-                f"Epoch: {epoch}, Train_loss: {round(loss.item(), 3)} Val_loss: {round(val_loss.item(), 3)}"
+                f"Epoch: {epoch}, average_train_loss: {round(avg_loss.item(), 3)} average_val_loss: {round(avg_val_loss.item(), 3)}"
             )
+
+
+def classifiers_fit(classifiers, x_train, y_train):
+    """Fit a list of classifiers each to classify one label in the output"""
+    assert len(classifiers) == y_train.shape[1]
+    for clf, y_label in zip(classifiers, y_train.T):
+        clf.fit(x_train, y_label)
